2894eaa5398bde3b906ad18b07b40523fb478caf
Aniket Pareshbhai Solanki <92724192+aniketsol456@users.noreply.github.com>
2024-03-19 22:22:14 +0530

    Aniket

--- cafe_management_system/src/App.jsx
+++ cafe_management_system/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './component/Navbar'
+import Home from './component/Home'
 
 const App = () => {
   return (
     <div className='container'>
       <Navbar/>
+      <Home/>
     </div>
   )
 }
---
+++ cafe_management_system/src/component/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import rice from "../assets/images/4.jpeg";
+
+const Home = () => {
+  return (
+    <div className="home-container">
+        <div className="greetings">
+            <h1>Welcome to our Cafe!</h1>
+            <p>Experience the taste of our delicious dishes.</p>
+        </div>
+        <div className="dish-photo">
+            <img src={rice} alt=""/>
+        </div>
+    </div>
+  
+
+    )
+}
+
+export default Home
