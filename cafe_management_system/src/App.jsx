6b69dd30c6a0434938869307741e3a7236cb2e9a
Aniket Pareshbhai Solanki <92724192+aniketsol456@users.noreply.github.com>
2024-04-03 23:53:41 +0530

    Aniket

--- cafe_management_system/src/App.jsx
+++ cafe_management_system/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './component/Navbar';
 import Home from './component/Home';
+import Signup from './component/Signup';
 
 const App = () => {
   return (
     <div className='container'>
-      <Navbar/>
-      <Home/>
+       <Navbar/>
+      {/* <Home/> */}
+      <Signup/>
     </div>
   )
 }
--- cafe_management_system/src/component/Signup.jsx
+++ cafe_management_system/src/component/Signup.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react';
+import User_icon from "../assets/images/user_icon.png"
+import Mail_icon from "../assets/images/mail_icon.png"
+import Pass_icon from "../assets/images/password_icon.png"
+import "../component/Signup.css";
 
 const Signup = () => {
+
+  const[action,setAction] = useState("Login");
+
   return (
-    <div>
-      
+    <div className="containe">
+      <div className="header">
+        <div className="text">{action}</div>
+        <div className="underline"></div>
+      </div>
+      <div className="inputs">
+        <div className="input">
+          <img src={User_icon} alt=""/>
+          <input type="text" placeholder='Name'/>
+        </div>
+        <div className="input">
+          <img src={Mail_icon} alt=""/>
+          <input type="text" placeholder="EMail Id"/>
+        </div>
+        <div className="input">
+          <img src={Pass_icon} alt=""/>
+          <input type="text" placeholder='Password'/>
+        </div>
+      </div>
+      <div className="forgo-pass">Lost Password?<span>Click Here!</span> </div>
+      <div className="submit">
+        <div className={action==="Login"?"submit gray":"submit"}>Sign Up</div>
+        <div className={action==="Sign Up"?"submit gray":"submit"}>Login</div>
+      </div>
     </div>
   )
 }
