597296259cbc1c70843099ec50754beecaf090a1
Aniket Pareshbhai Solanki <92724192+aniketsol456@users.noreply.github.com>
2024-05-01 10:54:23 +0530

    Aniket

--- cafe_management_system/src/component/Navbar.jsx
+++ cafe_management_system/src/component/Navbar.jsx
@@ -3,56 +3,49 @@ import './Navbar.css';
 import search_logo from '../assets/images/search_2.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
-// import {faBars} from '@fortawesome/free-solid-svg-icons';
-import {Link} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate
 
-const Navbar = () => {
-  const [navClassname,setNavclassname] = useState('navitems');
+const Navbar = ({ cart, setCart }) => {
+  const navigate = useNavigate(); // Initialize navigate function
+  const [navClassname, setNavclassname] = useState('navitems');
 
-  const togglemenu = ()=>{
-    if (navClassname==='navitems'){
-      setNavclassname('navitems responsive');
-    }
-    else{
-      setNavclassname('navitems');
-    }
-  }
+  const toggleMenu = () => {
+    setNavclassname(navClassname === 'navitems' ? 'navitems responsive' : 'navitems');
+  };
 
-
+  const openCartPage = () => { // Function to handle navigation to the Cart page
+    navigate('/cart'); // Use navigate to change route
+  };
 
   return (
     <div className='navBar'>
-
         <h1>Cafe<span>.</span></h1>
-
-        <div className="menutoggle">
+        <div className="menutoggle" onClick={toggleMenu}>
           <span></span>
           <span></span>
           <span></span>
         </div>
-
         <div className={navClassname}>
           <ul>
-            <Link to="/"> Home </Link>
-            <Link to="/menu"> Menu </Link>
-            <Link to="/galery"> Gallery </Link>
-            <Link to="/chef"> Chef </Link>
-            <Link to="/blog"> Blog </Link>
-            <Link to="/about"> About US </Link>
-            <Link to="/sign-up"> Signup/Login </Link>
+            <a href="/"> Home </a>
+            <a href="/menu"> Menu </a>
+            <a href="/gallery"> Gallery </a>
+            <a href="/chef"> Chef </a>
+            <a href="/blog"> Blog </a>
+            <a href="/about"> About US </a>
+            <a href="/sign-up"> Signup/Login </a>
           </ul>
         </div>
-
+        <FontAwesomeIcon icon={faShoppingCart} onClick={openCartPage} className='cartIcon'/>
         <div className='searchBox'>
             <input type="text" placeholder='Search'/>
-            <img src={search_logo} alt=""/>
+            <img src={search_logo} alt="Search"/>
         </div>
-        <FontAwesomeIcon icon={faShoppingCart} />
-        <div className="barIcon" onClick={togglemenu}>
+        <div className="barIcon" onClick={toggleMenu}>
           <FontAwesomeIcon icon={faBars}/>
         </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
