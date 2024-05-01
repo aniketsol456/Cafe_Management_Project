7304ab3301bf3b5da24058b48794c26964e6d13e
Aniket Pareshbhai Solanki <92724192+aniketsol456@users.noreply.github.com>
2024-05-01 10:54:15 +0530

    Aniket

---
+++ cafe_management_system/src/component/Cart.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Cart.css';
+
+const Cart = ({ cart, setCart }) => {
+  const increaseQty = (id) => {
+    setCart(cart.map(item => item.id === id ? {...item, qty: item.qty + 1} : item));
+  };
+
+  const decreaseQty = (id) => {
+    setCart(cart.map(item => 
+      item.id === id ? {...item, qty: item.qty - 1 > 0 ? item.qty - 1 : 1} : item
+    ));
+  };
+
+  return (
+    <div className="cart-page">
+      <h1>Your Cart</h1>
+      <ul>
+        {cart.map((item, index) => (
+          <li key={index}>
+            {item.name} - Rs.{item.price} x {item.qty}
+            <button onClick={() => increaseQty(item.id)}>+</button>
+            <button onClick={() => decreaseQty(item.id)}>-</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+
+  );
+}
+
+export default Cart;
--- cafe_management_system/src/component/Menu.jsx
+++ cafe_management_system/src/component/Menu.jsx
@@ -8,27 +8,36 @@ import Vanilla from '../assets/images/12.jpeg';
 
 const Menu = () => {
   const [selectedCategory, setSelectedCategory] = useState('All');
+  const [cart, setCart] = useState([]);
 
   const menuItems = [
     { id: 1, name: 'Margherita Pizza', category: 'Pizza', image: Margherita, price: 110 },
     { id: 2, name: 'Burger', category: 'Pizza', image: Burger, price: 120 },
     { id: 3, name: 'Masala Sandwich', category: 'Sandwichs', image: Masalasandwich, price: 50 },
-    { id: 4, name: 'Cool-Bar', category: 'Colddrinks', image:coolbar , price: 125 },
-    { id: 5, name: 'Vanilla Ice Cream', category: 'Icecreams', image:Vanilla, price: 45 },
-    // Add more items here
+    { id: 4, name: 'Cool-Bar', category: 'Colddrinks', image: coolbar, price: 125 },
+    { id: 5, name: 'Vanilla Ice Cream', category: 'Icecreams', image: Vanilla, price: 45 },
   ];
 
   const handleCategoryClick = (category) => {
     setSelectedCategory(category);
   };
 
+  const addToCart = (product) => {
+    const exist = cart.find(item => item.id === product.id);
+    if (exist) {
+      setCart(cart.map(item => 
+        item.id === product.id ? {...item, qty: item.qty + 1} : item
+      ));
+    } else {
+      setCart([...cart, {...product, qty: 1}]);
+    }
+  };
+
   const filteredItems = selectedCategory === 'All' ? menuItems : menuItems.filter(item => item.category === selectedCategory);
 
   return (
     <div className='Menu'>
-      <div className="title">
-        <h1>------------Menu------------</h1>
-      </div>
+      <div className="title"><h1>------------Menu------------</h1></div>
       <div className="Items">
         <ul>
           <li onClick={() => handleCategoryClick('All')}>All</li>
@@ -44,7 +53,7 @@ const Menu = () => {
             <img src={item.image} alt={item.name} />
             <h3>{item.name}</h3>
             <p>Rs.{item.price}</p>
-            <button>Add to Cart</button>
+            <button onClick={() => addToCart(item)}>Add to Cart</button>
           </div>
         ))}
       </div>
