66f22088bb69334ab0ebf318723f6292262c3199
Aniket Pareshbhai Solanki <92724192+aniketsol456@users.noreply.github.com>
2024-05-02 18:17:32 +0530

    Aniket

---
+++ cafe_management_system/src/component/Profile.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import crypto from 'crypto';
+import './Profile.css'
+
+const Profile = () => {
+  // Simulate user data
+  const userData = {
+    name: 'John Doe',
+    phone: '123-456-7890',
+    password: 'password123',
+    orders: ['order1', 'order2', 'order3']
+  };
+
+  const [encryptedPassword, setEncryptedPassword] = useState('');
+
+  useEffect(() => {
+    const hash = crypto.createHash('sha256');
+    hash.update(userData.password);
+    setEncryptedPassword(hash.digest('hex'));
+  }, [userData.password]);
+
+
+  return (
+    <div className='profile'>
+      <h1>User Profile</h1>
+      <p><strong>Name:</strong> {userData.name}</p>
+      <p><strong>Phone Number:</strong> {userData.phone}</p>
+      <p><strong>Encrypted Password:</strong> {encryptedPassword}</p>
+      <p><strong>Order History Count:</strong> {userData.orders.length}</p>
+    </div>
+  );
+}
+
+export default Profile;
