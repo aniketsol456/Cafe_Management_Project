2e5221e9734d1ab4682097c883f80d59ad6f1439
Aniket Pareshbhai Solanki <92724192+aniketsol456@users.noreply.github.com>
2024-05-02 12:00:32 +0530

    Aniket

--- cafe_management_system/src/component/Footer.jsx
+++ cafe_management_system/src/component/Footer.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Footer.css'; // Importing CSS file for styling
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
   return (
@@ -8,10 +10,10 @@ const Footer = () => {
       <div className="footer-content">
         <div className="footer-section about">
           <h2>About Us</h2>
-          <p>Your cafe's description goes here...</p>
+          <p>Cafe.,Nr.parevdi chowk,Rajkot,360005.</p>
           <div className="contact">
-            <span><i className="fas fa-phone"></i> 123-456-7890</span>
-            <span><i className="fas fa-envelope"></i> info@yourcafe.com</span>
+            <span><FontAwesomeIcon icon={faPhone} /> +91 9054214277</span>
+            <span><FontAwesomeIcon icon={faEnvelope} /> cafe.07@gmail.com</span>
           </div>
         </div>
         <div className="footer-section links">
@@ -26,9 +28,9 @@ const Footer = () => {
         <div className="footer-section social">
           <h2>Follow Us</h2>
           <div className="social-icons">
-            <a href="#"><i className="fab fa-facebook"></i></a>
-            <a href="#"><i className="fab fa-twitter"></i></a>
-            <a href="#"><i className="fab fa-instagram"></i></a>
+            <a href="#"><FontAwesomeIcon icon={faFacebook} /></a>
+            <a href="#"><FontAwesomeIcon icon={faTwitter} /></a>
+            <a href="#"><FontAwesomeIcon icon={faInstagram} /></a>
           </div>
         </div>
       </div>
