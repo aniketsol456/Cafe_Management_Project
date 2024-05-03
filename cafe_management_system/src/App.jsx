75f6940130af7d3c972fe762c4124f151d39e268
ZalaRushirajsinh <zalarushirajsinh07@gmail.com>
2024-05-03 08:00:47 +0530

    Rushiraj

--- cafe_management_system/src/App.jsx
+++ cafe_management_system/src/App.jsx
@@ -22,9 +22,9 @@ const App = () => {
     <BrowserRouter>
     <>
     <div className='container'>
-      {/* <Navbar/> */}
+      <Navbar/>
       <Routes>
-        {/* <Route exact path="/" element={ <Home/>}/>
+        <Route exact path="/" element={ <Home/>}/>
         <Route exact path="/menu" element={ <Menu/>}/>
         <Route exact path="/gallery" element={ <Gallery/>}/>
         <Route exact path="/chef" element={ <Chef/>}/>
@@ -32,11 +32,11 @@ const App = () => {
         <Route exact path="/feedback" element={ <Feedback/>}/>
         <Route exact path="/sign-up" element={ <Signup/>}/>
         <Route exact path="/cart" element={<Cart/>}/>
-        <Route exact path='/profile' element={<Profile/>}/> */}
-        <Route exact path = '/' element={<Dashboard/>}/>
+        <Route exact path='/profile' element={<Profile/>}/>
+        {/* <Route exact path = '/' element={<Dashboard/>}/>
         <Route exact path = '/orders' element={<Order/>}/>
         <Route exact path = '/customers' element={<Customer/>}/>
-        <Route exact path = '/manu' element={<Menumanage/>}/>
+        <Route exact path = '/manu' element={<Menumanage/>}/> */}
       </Routes>
     </div>
     </>
