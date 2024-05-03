6436b0613cb70497bf4aaaa802b050e24d86b74c
ZalaRushirajsinh <zalarushirajsinh07@gmail.com>
2024-05-03 08:01:37 +0530

    Rushiraj

--- cafe_management_system/src/component/Gallery.jsx
+++ cafe_management_system/src/component/Gallery.jsx
@@ -1,12 +1,39 @@
 import React from 'react';
-import './Gallery.css';
+import './Gallery.css'; // Import the CSS file
+import I1 from '../assets/images/17.jpg';
+import I2 from '../assets/images/18.jpg';
+import I3 from '../assets/images/19.jpg';
+import I4 from '../assets/images/20.jpg';
 
 const Gallery = () => {
+  const imagePaths = [I1, I2, I3, I4];
+
+  const getRandomWidth = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
+  const renderImage = (imagePath) => {
+    const width = getRandomWidth(200, 400);
+    const height = getRandomWidth(400, 500);
+
+    return (
+      <img
+        key={imagePath}
+        src={imagePath}
+        alt="Gallery Image"
+        style={{ width: `${width}px`, height: `${height}px`, margin: '10px' }}
+      />
+    );
+  };
+
   return (
     <div className='Gallery'>
-      <h1>**Memories**</h1>
+      <h1>Memories</h1>
+      <div className="image-container">
+        {imagePaths.map((path) => renderImage(path))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Gallery
+export default Gallery;
