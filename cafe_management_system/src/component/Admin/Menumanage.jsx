36beeaa8302282b5a0b901a99045a0b1bf4bfa83
Aniket Pareshbhai Solanki <92724192+aniketsol456@users.noreply.github.com>
2024-05-03 07:42:59 +0530

    Aniket

--- cafe_management_system/src/component/Admin/Menumanage.jsx
+++ cafe_management_system/src/component/Admin/Menumanage.jsx
@@ -1,19 +1,35 @@
-import React from 'react'
+import React from 'react';
 import Sidebar from './Sidebar';
 import Footer from './Footer';
 import '../Admin/Menumanage.css';
 
 const Menumanage = () => {
     return (
-        <><div className="menumanage">
-              <Sidebar />
-              <div className="main-content">
-                  <h1>Menu</h1>
-              </div>
-          </div>
-          <Footer/>
-          </>
-      );
+        <>
+            <div className="menumanage">
+                <Sidebar />
+                <div className="main-content">
+                    <h1>Menu Management</h1>
+                    <form className="menu-form">
+                        <div className="form-group">
+                            <label htmlFor="item_name">Item Name:</label>
+                            <input type="text" id="item_name" name="item_name" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="item_picture">Item Picture:</label>
+                            <input type="file" id="item_picture" name="item_picture" accept="image/*" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="item_price">Item Price (in Rupees):</label>
+                            <input type="number" id="item_price" name="item_price" min="0" />
+                        </div>
+                        <button type="submit">Add Item</button>
+                    </form>
+                </div>
+            </div>
+            <Footer />
+        </>
+    );
 }
 
-export default Menumanage
+export default Menumanage;
